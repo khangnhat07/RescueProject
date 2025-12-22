d1598cbc406a46ea0a6978f5e75d9aed987cabba
trunggcao <caoquoctrung2004@gmail.com>
2025-12-22 19:49:07 +0700

    update backend (with login) 1.0

--- RescueProject/src/main/java/com/example/RescueProject/service/Impl/UserServiceImpl.java
+++ RescueProject/src/main/java/com/example/RescueProject/service/Impl/UserServiceImpl.java
@@ -1,4 +1,17 @@
 package com.example.RescueProject.service.Impl;
 
-public class UserServiceImpl {
+import com.example.RescueProject.model.User;
+import com.example.RescueProject.repository.UserRepository;
+import com.example.RescueProject.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
 }
